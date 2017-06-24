b6958013ae845ca66d62b0ba471c66273057d7ce
Zerk <axzerk@gmail.com>
2017-06-25 01:20:26 +0300

    changes to eslintrc conserning airbnb preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,14 +21,6 @@ module.exports = {
       "error",
       "unix"
     ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
   },
   "plugins": [
     "react",
