31755512de8a8ecec24977dc9f9babb2da18d7ff
Zerk <axzerk@gmail.com>
2017-06-25 04:26:29 +0300

    updated eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
   "extends": "airbnb",
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
-  },
   "rules": {
+    "max-len": [1, 100, 2, {ignoreComments: true}],
+    "quote-props": [1, "consistent-as-needed"],
+    "no-unused-vars": [2, {"args": "none"}],
+    "radix": 0,
     "func-names": ["error", "never"],
     "linebreak-style": [
       "error",
       "unix"
     ],
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
   },
   "plugins": [
     "react",
