e1fba43ce10f0c2d4a81235b9815ab9201e56b6f
Alexander Repeta <axzerk@gmail.com>
2018-01-19 21:07:30 +0200

    updates react to 16.2 and some minor eslint\banel config tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,28 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true
   },
-  "extends": "airbnb",
-  "parser": "babel-eslint",
-  "rules": {
-    "max-len": [1, 100, 2, { ignoreComments: true }],
-    "quote-props": [1, "consistent-as-needed"],
-    // "no-unused-vars": [2, {"args": "none"}],
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  rules: {
+    'max-len': [1, 80, 2, { ignoreComments: false }],
+    'quote-props': [1, 'consistent-as-needed'],
     'no-unused-vars': ['warn'],
-    "radix": 0,
-    "func-names": ["error", "never"],
-    "linebreak-style": [ "error", "windows" ], // change "windows" for "unix" if your are on Mac
+    radix: 0,
+    'func-names': ['error', 'never'],
+    'linebreak-style': ['error', 'windows'], // change "windows" for "unix" if your are on Mac
     'no-console': 0,
+    // 'global-require': 'error'
   },
-  "plugins": [
-    "react",
-    "jsx-a11y",
-    "import"
-  ],
-  "settings": {
-    "import/resolver": {
-      "webpack": {
-        "config": "webpack.config.js"
+  plugins: ['react', 'jsx-a11y', 'import'],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'webpack.config.js'
       }
     }
   },
-  "globals": {},
+  globals: {}
 };
